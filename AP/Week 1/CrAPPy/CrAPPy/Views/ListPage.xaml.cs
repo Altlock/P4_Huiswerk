922ddd4eb2914a919d10db2cab485da7b63540a9
Altlock <altlockg@gmail.com>
2019-04-30 13:11:46 +0200

    AP Assig. 1 done

---
+++ AP/Week 1/CrAPPy/CrAPPy/Views/ListPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CrAPPy.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListPage : ContentPage
+    {
+		public ListPage (IEnumerable<string> l)
+        {
+            InitializeComponent ();
+            Content = new ListView {ItemsSource = l};
+        }
+	}
+}
---
+++ AP/Week 1/CrAPPy/CrAPPy/Views/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CrAPPy.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        private List<string> nameList;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void ShowButton_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new ListPage(nameList));
+        }
+
+        private void AddButton_OnClicked(object sender, EventArgs e)
+        {
+            if (NameEntry.Text.Length != 0)
+            {
+                nameList.Add(NameEntry.Text);
+                return;
+            }
+            DisplayAlert(
+                "Lege invulbox",
+                "Als je iets wilt toevoegen aan de lijst, moet je iets in de invulbox typen.",
+                "Ok");
+        }
+    }
+}
